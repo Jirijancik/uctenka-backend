084aba351dfcf908cb2ed989f1a1c5688068bbe9
Jiri Jancik <jirijancik2@gmail.com>
2021-09-25 23:39:22 +0200

    workingserver

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,8 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import permissions from './permissions';
 import resolvers from './graphql/resolvers/resolvers';
 import typeDefs from './graphql/type_defs/typeDefs';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import * as http from 'http';
 
 import * as Koa from 'koa';
 import * as koaBody from 'koa-body';
@@ -13,107 +15,42 @@ import * as graphqlHTTP from 'koa-graphql';
 import * as Router from 'koa-router';
 import * as dotenv from 'dotenv';
 
-const port = 4000;
-const app = new Koa();
+async function startApolloServer(typeDefs, resolvers) {
+  const httpServer = http.createServer();
 
-app.use(
-  jwt({
-    secret: 'SUPER_SECRET',
-    algorithms: ['HS256'],
-    //  credentialsRequired: false,
-  })
-);
+  const server = new ApolloServer({
+    schema: applyMiddleware(
+      makeExecutableSchema({ typeDefs, resolvers }),
+      permissions
+    ),
+    context: ({ req }) => {
+      const user = req.user || null;
+      return { user };
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-const server = new ApolloServer({
-  schema: applyMiddleware(
-    makeExecutableSchema({ typeDefs, resolvers }),
-    permissions
-  ),
-  context: ({ req }) => {
-    const user = req.user || null;
-    return { user };
-  },
-});
+  await server.start();
+  const app = new Koa();
 
-server.applyMiddleware({ app });
+  app.use(
+    jwt({
+      secret: 'SUPER_SECRET',
+      algorithms: ['HS256'],
+      passthrough: true,
+    })
+  );
 
-app.listen({ port }, () => {
-  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
-});
+  server.applyMiddleware({ app });
 
-// import * as Koa from 'koa';
-// import * as koaBody from 'koa-body';
-// import * as cors from '@koa/cors';
-// import * as graphqlHTTP from 'koa-graphql';
-// import * as Router from 'koa-router';
-// import * as dotenv from 'dotenv';
-// import { ApolloServer } from 'apollo-server-koa';
+  httpServer.on('request', app.callback());
 
-// import * as jwt from 'jsonwebtoken';
+  await new Promise((resolve: any) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
 
-// import { connectDatabase } from './db';
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 
-// import { config } from './config';
-// //import { createRoutes } from './middlewares/routes';
-// import { schema } from './graphql';
-
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-//   credentials: true,
-// };
-
-// const app = new Koa();
-// const router = new Router();
-
-// dotenv.config();
-
-// app.use(koaBody());
-// app.use(cors(corsOptions));
-
-// // router.all(
-// //   '/graphql',
-// //   graphqlHTTP({
-// //     schema,
-// //     graphiql: true,
-// //     context,
-// //   })
-// // );
-
-// app.use(router.routes()).use(router.allowedMethods());
-
-// const context = ({ req }: any) => {
-//   const token = req.cookies['jwt'] || '';
-//   try {
-//     return ({ id, email } = jwt.verify(token, SECRET_KEY));
-//   } catch (e) {
-//     throw new AuthenticationError(
-//       'Authentication token is invalid, please log in'
-//     );
-//   }
-// };
-
-// const server = new ApolloServer({
-//   typedefs,
-//   resolvers,
-//   context,
-// });
-// server.applyMiddleware({ app });
-
-// //app.use(createRoutes());
-
-// //export const server = app.listen(config.port);
-
-// (async () => {
-//   try {
-//     const info = await connectDatabase(process.env.DB_CONNECT);
-//     console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
-//   } catch (error) {
-//     console.error(
-//       'Unable to connect to database, uri: ',
-//       process.env.DB_CONNECT
-//     );
-//   }
-
-//   app.listen(config.port);
-//   console.log(`Server started on port ${config.port}`);
-// })();
+  return { server, app };
+}
+startApolloServer(typeDefs, resolvers);
--- server/graphql/type_defs/typeDefs.ts
+++ server/graphql/type_defs/typeDefs.ts
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server-express";
+import { gql } from 'apollo-server-koa';
 
 export default gql`
   type User {
